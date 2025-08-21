62ef3179ecdedc1f70d13ee0298bcaad5d1e6dab
actually-enkay <itzenkay@gmail.com>
2025-08-21 13:48:27 +0000

    Changed folder structure used django to make a better folder structure for backend devs

--- dear_journal/asgi.py
+++ dear_journal/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for journal_app project.
+ASGI config for dear_journal project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'journal_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dear_journal.settings')
 
 application = get_asgi_application()
