edd24cc37a0104d995e689cfb89a97c7dcdc0fb9
Umah <anonymousumah96@gmail.com>
2025-08-23 17:32:04 +0000

    added database entries to the database to the dashboard view

--- dear_journal_app/urls.py
+++ dear_journal_app/urls.py
@@ -9,8 +9,15 @@ urlpatterns = [
     # the dashboard 
     path("dashboard/",views.dashboard_view,name = "dashboard"),
 
+    # Admin Database 
+    path("dashboard/database/",views.database , name = 'database' ),
+
     # Login and logout
     path("login/", auth_views.LoginView.as_view(template_name="adminLogin.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+
+
+    
+
 
 ]
