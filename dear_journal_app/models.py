75e5337af7bde33e9e62982ee8fe99d1e028a072
Umah <anonymousumah96@gmail.com>
2025-08-24 18:32:29 +0000

    created create_profiles for frontend devs to retrieve their info and store it to a model called frontend developers.

--- dear_journal_app/models.py
+++ dear_journal_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#  this model is for front end devs
+
+class FrontendDeveloper (models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
+    bio = models.TextField(blank = True , null = True)
+    skills = models.CharField(max_length=255, blank = True , null=True)
+    profile_picture = models.ImageField(upload_to='frontend_profiles/', blank=True,null=True)
+
+    def __str__(self):
+        return f" {self.user.username}'s Frontend Profile"
